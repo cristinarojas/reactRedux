29bd20cfb4b0433cac5e22425594d86147e734d9
Cristina Rojas <telematicacristina@gmail.com>
2018-09-20 17:36:06 -0500

    Adding example of store

--- params/src/index.js
+++ params/src/index.js
@@ -2,14 +2,32 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+
+// Redux Store.
+import configureStore from './shared/redux/configureStore';
 
 // Routes
 import AppRoutes from './routes';
 
-render(
-  <Router>
-    <AppRoutes />
-  </Router>,
-  document.getElementById('root')
-);
+// Configuring Redux Store.
+const store = configureStore(window.initialState);
+
+// DOM.
+const rootElement = document.getElementById('root');
+
+// App wrapper
+const renderApp = Component => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <Component />
+      </Router>
+    </Provider>,
+    rootElement
+  );
+}
+
+// rendering our App
+renderApp(AppRoutes);
